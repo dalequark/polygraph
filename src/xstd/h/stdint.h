37d861fac20625aeaac30462e17117bb18d595a1
dale <damarkowitz@gmail.com>
2014-07-17 06:07:08 -0400

    Compiled for fedora

---
+++ src/xstd/h/stdint.h
@@ -0,0 +1,9 @@
+#ifndef _POLYGRAPH_SRC_XSTD_H_STDINT_H
+#define _POLYGRAPH_SRC_XSTD_H_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "polygraph 4.3.2"
+/* generated using gnu compiler gcc (GCC) 4.6.2 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
